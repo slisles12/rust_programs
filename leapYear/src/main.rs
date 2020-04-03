85628db30092d5f21b62475e78c2cdd70487c707
slisles12 <lislees17@gcc.edu>
2020-04-03 18:10:52 -0400

    leap year program

--- leapYear/src/main.rs
+++ leapYear/src/main.rs
@@ -1,3 +1,19 @@
+use std::io; 
+
 fn main() {
-    println!("Hello, world!");
+
+    let mut year = String::new();
+
+    println!("Please enter a year: ");
+
+    io::stdin().read_line(&mut year).expect("failed to read from stdin");
+
+    let year : i32 = year.trim().parse().expect("invalid input");
+
+    if year % 4 != 0 {
+        println!("{} is not a leap year!", year);
+    }
+    else{
+        println!("{} is a leap year!", year);
+    }
 }
